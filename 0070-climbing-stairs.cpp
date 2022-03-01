06099786723328e72449c0769d3c082a48e06bb1
colackwingm <xuanshu.luo@live.com>
2022-03-01 17:43:00 +0100

    220301 update

---
+++ 0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int res[46];
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        res[1] = 1;
+        res[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            res[i] = res[i-1] + res[i-2];
+        }
+        return res[n];
+    }
+};
